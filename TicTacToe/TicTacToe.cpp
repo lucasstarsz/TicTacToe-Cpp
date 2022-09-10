635a7125db95ac561c6aad0f2a61430ecd1fe2d7
lucasstarsz <andrewrcdey@gmail.com>
2022-09-10 14:01:24 -0400

    add basic game loop

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -4,9 +4,49 @@
 
 #include <string>
 #include <iostream>
+#include <chrono>
 
 namespace TicTacToe
 {
+
+	namespace Utilities
+	{
+		std::string getInput(std::string prompt)
+		{
+			std::string result;
+
+			do
+			{
+				std::cout << prompt;
+				std::getline(std::cin, result);
+
+				if (result.empty())
+				{
+					continue;
+				}
+
+				bool isValid = false;
+
+				for (int i = 0; i < result.length(); i++)
+				{
+					if (result[i] != ' ')
+					{
+						isValid = true;
+						break;
+					}
+				}
+
+				if (isValid)
+				{
+					break;
+				}
+
+			} while (true);
+
+			return result;
+		}
+	}
+
 	struct Player
 	{
 		int number;
@@ -111,49 +151,35 @@ namespace TicTacToe
 
 		board.print();
 
+		// I mean... It's fine?
+		// I should look farther into that C++11 api...
+		const std::chrono::nanoseconds timeSeed = std::chrono::duration_cast<std::chrono::nanoseconds>(
+			std::chrono::system_clock::now().time_since_epoch()
+		);
+
+		std::srand(timeSeed.count());
+
+		const int firstPlayerTurn = (std::rand() % 2); std::cout << firstPlayerTurn;
+
+		Player currentPlayer = firstPlayerTurn % 2 == 0 ? *player2 : *player1;
+		std::string playerCommand;
+
 		while (!board.hasWinner())
 		{
 			// TODO: notify player turn
 			// TODO: wait for command
+			playerCommand = TicTacToe::Utilities::getInput(*currentPlayer.name + "'s turn: ");
+
 			// TODO: process command
+			std::cout << "Received command from " << *currentPlayer.name << ": '" << playerCommand << "\'\n";
+
 			// TODO: show board
+			board.print();
+			
+			// TODO: change player
+			currentPlayer = currentPlayer.number == 1 ? *player2 : *player1;
 		}
 	}
-}
-
-std::string getInput(std::string prompt)
-{
-	std::string result;
-
-	do
-	{
-		std::cout << prompt;
-		std::getline(std::cin, result);
-
-		if (result.empty())
-		{
-			continue;
-		}
-
-		bool isValid = false;
-
-		for (int i = 0; i < result.length(); i++)
-		{
-			if (result[i] != ' ')
-			{
-				isValid = true;
-				break;
-			}
-		}
-
-		if (isValid)
-		{
-			break;
-		}
-
-	} while (true);
-
-	return result;
 }
 
 int main()
@@ -163,8 +189,8 @@ int main()
 	std::string player1Name;
 	std::string player2Name;
 
-	player1Name = getInput("Player 1's name: ");
-	player2Name = getInput("Player 2's name: ");
+	player1Name = TicTacToe::Utilities::getInput("Player 1's name: ");
+	player2Name = TicTacToe::Utilities::getInput("Player 2's name: ");
 
 	TicTacToe::Player player1 = { 1, &player1Name };
 	TicTacToe::Player player2 = { 2, &player2Name };
