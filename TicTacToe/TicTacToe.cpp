515dc6b13f0c9a8cf76bdb3da64fbb34a959f2db
lucasstarsz <andrewrcdey@gmail.com>
2022-09-10 14:00:28 -0400

    improve board print aesthetic

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -96,15 +96,11 @@ namespace TicTacToe
 
 		void printBoardContents()
 		{
-			for (int i = 0; i < 3; i++)
-			{
-				for (int j = 0; j < 3; j++)
-				{
-					std::cout << board[i][j];
-				}
-
-				std::cout << "\n";
-			}
+			std::cout << "\n " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << " \n";
+			std::cout << "-----------\n";
+			std::cout << ' ' << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << " \n";
+			std::cout << "-----------\n";
+			std::cout << ' ' << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << " \n\n";
 		}
 	};
 
