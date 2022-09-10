a2e032f8a51693edb8c90f99a28a67f6813817c4
lucasstarsz <andrewrcdey@gmail.com>
2022-09-09 23:49:29 -0400

    revert "memory improvement" for getInput

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -110,7 +110,7 @@ namespace TicTacToe
 	}
 }
 
-std::string * getInput(std::string prompt)
+std::string getInput(std::string prompt)
 {
 	std::string result;
 
@@ -142,7 +142,7 @@ std::string * getInput(std::string prompt)
 
 	} while (true);
 
-	return &result;
+	return result;
 }
 
 int main()
@@ -152,8 +152,8 @@ int main()
 	std::string player1Name;
 	std::string player2Name;
 
-	player1Name = *getInput("Player 1's name: ");
-	player2Name = *getInput("Player 2's name: ");
+	player1Name = getInput("Player 1's name: ");
+	player2Name = getInput("Player 2's name: ");
 
 	TicTacToe::Player player1 = { 1, &player1Name };
 	TicTacToe::Player player2 = { 2, &player2Name };
