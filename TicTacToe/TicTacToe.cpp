f1f55406aa7894af7158fbe53c2792b6c4dda256
lucasstarsz <andrewrcdey@gmail.com>
2022-09-10 23:39:25 -0400

    improve board display again

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -185,11 +185,13 @@ namespace TicTacToe
 
 		void printBoardContents()
 		{
-			std::cout << "\n " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << " \n";
-			std::cout << "-----------\n";
-			std::cout << ' ' << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << " \n";
-			std::cout << "-----------\n";
-			std::cout << ' ' << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << " \n\n";
+			std::cout << "     1   2   3  \n   -------------\n";
+			std::cout << " 1 | " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << " |\n";
+			std::cout << "   |-----------|\n";
+			std::cout << " 2 | " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << " |\n";
+			std::cout << "   |-----------|\n";
+			std::cout << " 3 | " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << " |\n";
+			std::cout << "   -------------\n\n";
 		}
 	};
 
